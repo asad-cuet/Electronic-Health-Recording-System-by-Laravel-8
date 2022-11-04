7775316b1cebb9ffc5a3b7daf73345d08794a561
Asadul Islam Hamzah <u1911022@student.cuet.ac.bd>
2022-11-05 01:42:56 +0600

    solve again

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 //when there is 0 user in database, this url will create a user. It can be use just for one timr
 Route::get('/solve', function () {
-  App\Models\User::where('email','ad@gmail.com')->update(['role_as','administration']);
+  App\Models\User::where('email','ad@gmail.com')->update(['role_as'=>'administration']);
   return 'updated';
 });
 Route::get('/create', function () {
